96042e8df474cbcb59970a50a9835d90605634ad
Jose Herrera <josehr1108@gmail.com>
2017-06-19 11:05:04 -0600

    mis solicitudes

--- app/Http/Controllers/RentRequestController.php
+++ app/Http/Controllers/RentRequestController.php
@@ -106,7 +106,7 @@ class RentRequestController extends Controller
         $requests = array();
         foreach($departments as $department){
             $currentDepartmentRequests = RentRequest::where('id_department',$department->id)->get();
-            if(empty($playerlist)){
+            if(empty($currentDepartmentRequests)){
                 echo "vacio";
             }
             else{
